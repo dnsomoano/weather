fc8fa35f47d611657634d8027c0c00e8a4abdcf8
Daniel N Somoano <dnsomoano@gmail.com>
2018-08-08 00:55:38 -0400

    geolocation, but no geocode capability

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
-const APP_ID = "&units=imperial&appid=ee6accf634c9e58a5b694bb8d6ef0eb8";
+const APP_ID = ",us&units=imperial&appid=ee6accf634c9e58a5b694bb8d6ef0eb8";
+
+
 const weatherRequest = () => {
+
   // Takes input from user
   const usrInput = document.querySelector(".search-box").value;
   // string interpolation
@@ -22,13 +25,34 @@ const weatherRequest = () => {
       const _name = document.createElement("li");
       const _temp = document.createElement("li");
       const _weather = document.createElement("li");
-      _name.textContent = weatherResults.name;
-      _temp.textContent = weatherResults.main.temp;
-      _weather.textContent = weatherResults.weather[0].main;
+      _name.textContent = "The city: " + weatherResults.name;
+      _temp.textContent = "The Temperature: " + weatherResults.main.temp;
+      _weather.textContent = "The forecast right now: " + weatherResults.weather[0].main;
       parent.appendChild(_name);
       parent.appendChild(_temp);
       parent.appendChild(_weather);
     })
-}
+    
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log(position);
+      const _geocode = "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + "," + position.coords.longitude +"&sensor=false";
+      fetch(_geocode)
+      .then(location => {
+        let latitude = location.coords.latitude;
+        let longitude = location.coords.latitude;
+        console.log(latitude);
+        console.log(longitude);
+        const parent = document.querySelector(".weather-results");
+        const _latitude = document.createElement("li");
+        const _longitude = document.createElement("li");
+        _latitude.textContent = latitude;
+        _longitude.textContent = longitude;
+        parent.appendChild(_latitude);
+        parent.appendChild(_longitude);
+      })
+    })
+};
 
+
+// document.querySelector(".search-box").addEventListener("keypress", weatherRequest);
 document.querySelector(".search-button").addEventListener("click", weatherRequest);
